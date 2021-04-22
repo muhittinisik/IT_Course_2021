857df903541ae7084e047940449aa5b4aeb8402a
muhittinisik <isik_m@outlook.com>
2021-04-23 02:02:30 +0300

    Dosya düzenlemesi

---
+++ 1_Lesson Notes/4_Python/Assignments/assignment_008_1_(password_reminder).py
@@ -0,0 +1,9 @@
+# Assignment-008/1 (Password Reminder)
+
+my_name = "metin"
+password = "Fr154278.."
+username = input("Enter your first name: ")
+if username == my_name:
+  print("Hello, {}! The password is: {}".format(username, password))
+else:
+  print("Hello, {}! See you later.".format(username))
